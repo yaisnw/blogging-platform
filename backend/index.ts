e55947e25c1b49b55685da7b4cbecbf5931f9e9c
Yasin <eliteyasin1@gmail.com>
2025-03-15 17:28:42 +0500

    Project initialisation

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express with TypeScript!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
