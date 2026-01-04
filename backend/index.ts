6c4a889bf550c85550439cd170fb213eb3f9cb14
unknown <eliteyasin1@gmail.com>
2026-01-04 18:45:51 +0500

    changed backend cors policy

--- backend/index.ts
+++ backend/index.ts
@@ -46,7 +46,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors(
   {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
   }
 ))
