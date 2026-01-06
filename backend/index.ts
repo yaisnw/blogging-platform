eba8204985ecb019dba90974171981b48fc3da0e
unknown <eliteyasin1@gmail.com>
2026-01-06 15:50:11 +0500

    bypassing middleware stack for solution

--- backend/index.ts
+++ backend/index.ts
@@ -42,14 +42,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(
-  {
-    origin: process.env.FRONTEND_URL || 'https://blogging-platform-pearl-six.vercel.app',
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://blogging-platform-pearl-six.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
   }
-))
+  next();
+});
 
 app.options('*', cors());
 app.set('trust proxy', 1);
