2a3d4f5ba7b653592bde050bf11d8a275ff9fcb4
unknown <eliteyasin1@gmail.com>
2026-01-08 13:47:06 +0500

    changed main element for post editor to fix unsticky toolbar

--- frontend/src/components/templates/PostEditorTemplate.tsx
+++ frontend/src/components/templates/PostEditorTemplate.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "@/styles/editor.css";
 
 type Props = {
   children: React.ReactNode[] | React.ReactNode;
 };
 
 const PostEditorTemplate: React.FC<Props> = ({ children }) => {
-  return <main>{children}</main>;
+  return <main className="mainEditor">{children}</main>;
 };
 
 export default PostEditorTemplate;
