b29adc3915ad21c3eaae97e9ff10c70ba842a723
unknown <eliteyasin1@gmail.com>
2026-01-12 17:36:50 +0500

    since hosting issues is fixed, changed layout of postpanel on publicpostspage

--- frontend/src/components/molecules/PostPanel.tsx
+++ frontend/src/components/molecules/PostPanel.tsx
@@ -1,6 +1,7 @@
 import AppButton from "../atoms/AppButton"
 import styles from '../../styles/dashboardPage.module.css'
 import { AnimatePresence, motion } from "motion/react";
+import AppHeadingTwo from "../atoms/AppHeadingTwo";
 
 type postPanelProps = {
     createButton: () => void;
@@ -12,14 +13,14 @@ type postPanelProps = {
     isDashboard?: boolean;
 }
 
-const PostPanel: React.FC<postPanelProps> = ({ 
-    createButton, 
-    deleteButton, 
-    confirmDeleteButton, 
-    isDeleting, 
-    deletingPostIds, 
+const PostPanel: React.FC<postPanelProps> = ({
+    createButton,
+    deleteButton,
+    confirmDeleteButton,
+    isDeleting,
+    deletingPostIds,
     onSortChange,
-    isDashboard 
+    isDashboard
 }) => {
     return (
         <motion.nav
@@ -28,51 +29,53 @@ const PostPanel: React.FC<postPanelProps> = ({
             transition={{ duration: 0.4 }}
             className={styles.panel}
             aria-label="Post actions">
-            
-            <div className={styles.sectionTitle}>Actions</div>
-            <AnimatePresence mode="popLayout">
-                <AppButton key="create-btn" onClick={createButton}>Create Post</AppButton>
-                
-                {deleteButton && (
-                    <AppButton 
-                        key="delete-btn" 
-                        variant={isDeleting ? "secondary" : "primary"} 
-                        onClick={deleteButton}
-                    >
-                        {isDeleting ? 'Cancel Delete' : 'Delete Posts'}
-                    </AppButton>
-                )}
 
-                {isDeleting && (
-                    <motion.div
-                        initial={{ opacity: 0, y: -10 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -10 }}
-                        transition={{ duration: 0.3 }}
-                        key="confirm-delete-group"
-                        className={styles.deleteWrapper}
-                    >
+            <AppHeadingTwo className={styles.sectionTitle}>Actions</AppHeadingTwo>
+            <div>
+                <AnimatePresence mode="popLayout">
+                    <AppButton key="create-btn" onClick={createButton}>Create Post</AppButton>
+
+                    {deleteButton && (
                         <AppButton
-                            className={styles.deleteButton}
-                            variant="danger"
-                            disabled={deletingPostIds!.length === 0}
-                            showDisabledPopup
-                            onDisabledClick={() =>
-                                alert("Select at least one post before deleting.")
-                            }
-                            onClick={confirmDeleteButton}
+                            key="delete-btn"
+                            variant={isDeleting ? "secondary" : "primary"}
+                            onClick={deleteButton}
                         >
-                            Confirm Delete ({deletingPostIds?.length})
+                            {isDeleting ? 'Cancel Delete' : 'Delete Posts'}
                         </AppButton>
-                    </motion.div>
-                )}
-            </AnimatePresence>
+                    )}
 
-            
+                    {isDeleting && (
+                        <motion.div
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -10 }}
+                            transition={{ duration: 0.3 }}
+                            key="confirm-delete-group"
+                            className={styles.deleteWrapper}
+                        >
+                            <AppButton
+                                className={styles.deleteButton}
+                                variant="danger"
+                                disabled={deletingPostIds!.length === 0}
+                                showDisabledPopup
+                                onDisabledClick={() =>
+                                    alert("Select at least one post before deleting.")
+                                }
+                                onClick={confirmDeleteButton}
+                            >
+                                Confirm Delete ({deletingPostIds?.length})
+                            </AppButton>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+
+            </div>
+
 
             <div className={styles.sortSection}>
                 <label htmlFor="post-sort" className={styles.label}>Sort By</label>
-                <select 
+                <select
                     id="post-sort"
                     className={styles.select}
                     onChange={(e) => onSortChange(e.target.value)}
