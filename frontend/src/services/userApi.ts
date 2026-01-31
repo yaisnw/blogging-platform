7af83d830a9a0e9be3b7366f9e0bedef006b8222
unknown <eliteyasin1@gmail.com>
2026-01-31 14:04:06 +0500

    forgot to use baseapi in userApi file

--- frontend/src/services/userApi.ts
+++ frontend/src/services/userApi.ts
@@ -1,21 +1,8 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { responseUser } from "../types/rtkTypes";
+import { baseApi } from "./baseApi";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const userApi = createApi({
-    reducerPath: "userApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${BASE_URL}/user`,
-        prepareHeaders: (headers) => {
-            const token = localStorage.getItem("token");
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
-    }),
-    tagTypes: ["User"],
+export const userApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         getUser: build.query<responseUser, number>({
             query: (id) => `/${id}`,
